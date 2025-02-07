54aac5df6243a80f85f228769895fde6ba7367b4
Kaisar Ages Querio <92149901+kaisaragess@users.noreply.github.com>
2025-02-07 23:00:39 +0700

    menambahkan-tugas1-daspro

---
+++ tugas1-daspro.py
@@ -0,0 +1,15 @@
+print("                 TOKO MAINAN ANAK")
+print("             ***************************")
+
+nama = input("Masukkan Nama Pembeli : ")
+kode_mainan = input("Masukkan Kode Mainan : ")
+harga = int(input("Masukkan Harga : "))
+kuantitas = int(input("Masukkan Jumlah Beli : "))
+
+print("======================================")
+print("Nama Pembeli     = ", nama)
+print("Kode Mainan      = ", kode_mainan)
+print("Harga            = ", harga)
+print("Jumlah Beli      = ", kuantitas)
+print("Total            = ", harga * kuantitas)
+
